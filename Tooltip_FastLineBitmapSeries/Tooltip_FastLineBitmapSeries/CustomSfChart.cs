e2d4f6caf94653eb71e5e836af7c4151c60e4c42
AzureAD\SamuelRajaduraiEdwin <samuelrajaduraie@syncfusion.com>
2020-01-09 11:39:59 +0530

    Adding the sample in the repository

---
+++ Tooltip_FastLineBitmapSeries/Tooltip_FastLineBitmapSeries/CustomSfChart.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Tooltip_FastLineBitmapSeries
+{
+    public class CustomSfChart : SfChart
+    {
+        // This panel is a positional reference panel to update the tooltip.
+        public Canvas AdorningCanvas { get; set; }
+
+        // The adorning canvas is get in this method.
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            AdorningCanvas = GetTemplateChild("adorningCanvas") as Canvas;
+        }
+    }
+}
